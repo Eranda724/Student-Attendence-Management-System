ee487e846c5711695a042c2472b6e1d34e4764c1
Eranda724 <erandajayasinghe724@gmail.com>
2024-11-29 10:31:03 +0530

    update path

--- inc/MA/MA.php
+++ inc/MA/MA.php
@@ -31,6 +31,8 @@ if (isset($_POST['wh'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student Attendance Management System</title>
     <link rel="stylesheet" href="styles.css">
+
+
 </head>
 
 <body>
--- inc/connection.php
+++ inc/connection.php
@@ -1,5 +1,5 @@
 <?php 
-	$connection = mysqli_connect('localhost', 'root', '' ,'SA');
+	$connection = mysqli_connect('localhost', 'root', '' ,'mypro');
 
 	if(mysqli_connect_errno()){
 		die("error");
